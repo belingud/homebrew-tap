aec9d783682121ccb32aead4a59d7bda367be9af
Belingud <im.victor@qq.com>
2026-04-22 23:52:25 +0800

    Add gh-download formula

---
+++ Formula/gh-download.rb
@@ -0,0 +1,20 @@
+class GhDownload < Formula
+  desc "Download a file or directory from a GitHub repository path"
+  homepage "https://github.com/belingud/gh-download"
+  url "https://github.com/belingud/gh-download.git",
+      tag:      "v0.5.0",
+      revision: "6191f93c1fd92467d15ef808d69f29d5cc2fb6b3"
+  version "0.5.0"
+  license "MIT"
+  head "https://github.com/belingud/gh-download.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args, "--locked"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gh-download --version")
+  end
+end
