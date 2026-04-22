133341e7bebcff8500e992e97c0e30e69c1a0f20
Belingud <im.victor@qq.com>
2026-04-22 23:45:34 +0800

    Add gptcomet formula

---
+++ Formula/gptcomet.rb
@@ -0,0 +1,38 @@
+class Gptcomet < Formula
+  desc "AI-powered Git commit message generator and reviewer"
+  homepage "https://github.com/belingud/gptcomet"
+  url "https://github.com/belingud/gptcomet.git",
+      tag:      "v2.4.1",
+      revision: "186b456f3a941e93f184eda7db3658d34d8eeba1"
+  version "2.4.1"
+  license "MIT"
+  head "https://github.com/belingud/gptcomet.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "."
+    bin.install_symlink "gptcomet" => "gmsg"
+  end
+
+  def caveats
+    <<~EOS
+      GPTComet includes a built-in update command.
+      If you installed it with Homebrew, use this instead of `gmsg update`:
+
+        brew upgrade gptcomet
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gptcomet --version")
+    assert_match version.to_s, shell_output("#{bin}/gmsg --version")
+  end
+end
